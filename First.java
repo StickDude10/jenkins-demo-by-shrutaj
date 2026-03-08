96b30f938455835d76f7b0b2c823891a1d5dc82d
StickDude10 <109211242+StickDude10@users.noreply.github.com>
2026-03-09 01:34:46 +0530

    Update First.java

--- First.java
+++ First.java
@@ -1,5 +1,7 @@
 public class First { 
 public static void main(String[] args) { 
-System.out.println("Hello Java"); 
+System.out.println("Hello Java");
+System.out.println("New Line added");
+  
 } 
 } 
